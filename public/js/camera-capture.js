852bf629d2e2bcd0bf061e2859a855d72ccd9845
Salvador Roibon <sroibon@tradehelm.com>
2016-11-10 11:07:47 -0300

    Added jquery, added success alert

--- public/js/camera-capture.js
+++ public/js/camera-capture.js
@@ -9,6 +9,12 @@ function hasGetUserMedia() {
 if (hasGetUserMedia()) {
   // Good to go!
   console.log('You can use your camera!');
+  var successDiv = document.createElement('div');
+  successDiv.id = 'success';
+  successDiv.className = 'alert alert-success';
+
+  document.getElementsByTagName('body')[0].appendChild(successDiv);
+
 
 } else {
   console.log('getUserMedia() is not supported in your browser');
