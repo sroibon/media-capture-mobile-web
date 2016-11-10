bdc189ad3a6a0db350223e571a9318277bb727eb
Salvador Roibon <sroibon@tradehelm.com>
2016-11-10 10:58:35 -0300

    Added bootstrap

--- public/js/camera-capture.js
+++ public/js/camera-capture.js
@@ -1,1 +1,15 @@
 console.log('Working!');
+
+
+function hasGetUserMedia() {
+
+  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+if (hasGetUserMedia()) {
+  // Good to go!
+  console.log('You can use your camera!');
+
+} else {
+  console.log('getUserMedia() is not supported in your browser');
+}
